d033e4e75b744b68ca3e6467fb7d6094dbfecf40
Frank Luongo <frank@frankluongo.com>
2023-02-22 19:17:10 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Lenova Capital`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
